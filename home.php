ec95f2e81739dd3ecbc6df9a03cc5aea31c1cbd8
Galuh304 <galuhapriliano30@gmail.com>
2024-02-17 15:18:34 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,19 @@
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+
+
+
+        <div class="swiper-slide" style="background-image: url(images/image2.jpg);">
+            <div class="overlay-s"></div>
+            <div class="slider-caption">
+                <div class="inner-content">
+                    <h2>OPTIMASI STOK BARANG DENGAN ANALISIS PENJUALAN MENGGUNAKAN METODE ASSOCIATION RULE MINING DI TOKO HANDARI</h2>
+                    <p>APRIORI</p>
+                    <!-- <a href="#" class="main-btn white">View Projects</a> -->
+                </div> <!-- /.inner-content -->
+            </div> <!-- /.slider-caption -->
+        </div> <!-- /.swier-slide -->
+
+
+    </div> <!-- /.swiper-wrapper -->
+</div> <!-- /.swiper-container -->
